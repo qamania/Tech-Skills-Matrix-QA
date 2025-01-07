6584d75b639213c809597250932b44aaec76bb06
mikhailchub <Mikhail.Chub.mailing@gmail.com>
2025-01-07 02:53:50 +0200

    Restructured project to enable multilanguage functionality

--- convert.py
+++ convert.py
@@ -1,14 +1,17 @@
 from jinja2 import Template
 import os
 
-TEMPLATE = 'template.html'
-MATRIX = 'matrix'
-OUTPUT = 'web/index.html'
-number = 1
+LANGUAGES = ['en']
+DEFAULT_LANGUAGE = 'en'
+TEMPLATE_BASE = 'templates'
+MATRIX_BASE = 'MATRIX'
+OUTPUT_BASE = 'web'
+STATIC_PATH_DEFAULT = './'  # For default index.html
+STATIC_PATH_LANG = '../'  # For language-specific subfolders index.html
 
 
 def convert(file, num):
-    with open(file) as file:
+    with open(file, encoding='utf-8') as file:  # Specify UTF-8 encoding
         lines = file.readlines()
 
     data = {'category': {}}
@@ -34,15 +37,29 @@ def convert(file, num):
     return data
 
 
-if __name__ == '__main__':
+def generate_html_for_language(language, output_path, static_path):
+    number = 1
     data = []
-    files = os.listdir(MATRIX)
+    matrix_dir = os.path.join(MATRIX_BASE, language)
+    template_path = os.path.join(TEMPLATE_BASE, language, 'template.html')
+
+    files = os.listdir(matrix_dir)
     files.sort()
     for file in files:
         if file.endswith('.md'):
-            data.append(convert(os.path.join(MATRIX, file), number))
+            data.append(convert(os.path.join(matrix_dir, file), number))
             number += 1
-    template = Template(open(TEMPLATE).read())
+
+    template = Template(open(template_path, encoding='utf-8').read())
     levels = ((1, 'Trainee'), (2, 'Junior'), (3, 'Middle'), (4, 'Senior'), (5, 'Expert'))
-    with open(OUTPUT, 'w') as file:
-        file.write(template.render(data=data, levels=levels))
+    os.makedirs(output_path, exist_ok=True)
+    output_file = os.path.join(output_path, 'index.html')
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(template.render(data=data, levels=levels, language=language, static_path=static_path))
+
+
+if __name__ == '__main__':
+    for lang in LANGUAGES:
+        generate_html_for_language(lang, os.path.join(OUTPUT_BASE, lang), STATIC_PATH_LANG)
+        if lang == DEFAULT_LANGUAGE:
+            generate_html_for_language(lang, OUTPUT_BASE, STATIC_PATH_DEFAULT)
